a28247f41d753d7a3b4b9fd2b8c89c88089ecb34
hexagon16rpm <hexagon16.rpm@outlook.com>
2019-11-22 22:48:30 +0200

    Fri 22 Nov 22:48:30 EET 2019

---
+++ max300.cpp
@@ -0,0 +1,18 @@
+#include <iostream>;
+using namespace std;
+int main() {
+	cout << "enter three numbers to compare: \n" << endl;
+	int x,y,z;
+	cin >> x >> y >> z;
+	if (x > y > z) {
+		cout << "max = " << x << endl;
+		} 
+	else if (y>z) { 
+		cout << "max = " << y << endl;
+		}
+	else { 
+		cout << "max = " << z << endl;
+		}
+	
+	return 0;
+}
