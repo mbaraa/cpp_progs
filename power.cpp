7eed67a802315466d0b750bf92f5e64877b64c5d
Hexagon16 <hexagon16.rpm@outlook.com>
2019-12-03 00:35:55 -0800

    Create power.cpp

---
+++ power.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int p = 1 ,no,lol;
+	cout << "enter desired number";
+	cin >> no;
+	cout << "enter desired number";
+	cin >> no;
+
+	for (int i = 1; i <= lol; i++) {
+		p = p * 2;
+	}
+	cout << "power = " << p;
+	return 0;
+}
