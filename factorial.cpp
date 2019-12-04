99b4ba56aa15d5e186668c7cfebfb0dad6d92822
hexagon16rpm <hexagon16.rpm@outlook.com>
2019-12-04 14:03:46 +0200

    modified @ Wed  4 Dec 14:03:46 EET 2019

---
+++ factorial.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int n;
+	cout << "Input a number :\n";
+	cin >> n;
+	int r = n;
+	int  k = n - 1;
+	if (n==1 || n==0){
+		cout << n << "! = "<< "1" <<endl;
+		}
+	else { 
+		for (int f = n ; f > 1 ; f--){
+			n = n * k;
+			k--;
+		}
+		cout << r <<"! = "<< n << endl;
+		
+		}
+	return 0;
+}
+			
+	
