03dd3dceb645bc7a4c6ae032222e6c12019fa37e
Hexagon16 <hexagon16.rpm@outlook.com>
2019-12-10 00:16:40 -0800

    modified @ Tue 10 Dec 10:17:19 EET 2019

--- delayment_example.cpp
+++ delayment_example.cpp
@@ -4,11 +4,17 @@ operating system API. It is defined by the POSIX.1 standard, the base of the Sin
 and should therefore be available in any conforming (or quasi-conforming) \
 operating system/compiler (all official versions of UNIX, including macOS, Linux, etc.).
 */
+/* if using windows use this header 
+#include <windows.h>
+*/
 using namespace std;
 int main(){
 	int x;
 	cin >> x;
 	usleep(3000000);// this will wait 3 million micro seconds (3 seconds) then executes the next statement
+	/* if using windows 
+	Sleep(3000); // Note uppercase S , and time in milliseconds hence this is 3000 milliseconds
+	*/
 	cout << x;
 	return 0 ;\
 
