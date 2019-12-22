4598d997695b0548338320c0b6b4424f59e48ad3
Hexagon16 <hexagon16rpm@yandex.com>
2019-12-22 00:17:13 -0800

    created @ Sun 22 Dec 10:17:15 EET 2019

---
+++ students_marker.cpp
@@ -0,0 +1,29 @@
+//generalize me please b****
+#include<iostream>
+using namespace std;
+int main() {
+	char id[3] = {'n1','n2','n3'} ;
+	int first[3], second[3], total[3];
+	cout << "enter 1st exam mark\n";
+	for (int i = 0; i < 3; i++) {
+		cin >> first[i];
+	}	
+	cout << "enter 2nd exam mark\n";
+
+	for (int i = 0; i < 3; i++) {
+		cin >> second[i];
+	}
+	for (int i = 0; i < 3; i++) {
+		total[i] = first[i] + second[i];
+	}
+
+	int max = total[0] , loc = 0;
+	for (int i = 0; i < 3; i++) {
+		if (total[i] > max) {
+			max = total[i];
+			loc = i;
+		}
+	}
+	cout << "Max mark is : " << max << " of the student : " << id[loc];
+	return 0;
+}
