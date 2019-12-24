0876a71032b653366aa248254991296c26402c47
Hexagon16 <hexagon16rpm@yandex.com>
2019-12-23 23:10:58 -0800

    modified @ Tue 24 Dec 09:11:00 EET 2019

--- 2dim_array_e.g.cpp
+++ 2dim_array_e.g.cpp
@@ -4,12 +4,16 @@ int  main(){
 	int list[3][2];
 	int j ,i;
 	cout << "enter some numerical vsalus \n";
+	//this loop for rows
 	for(i = 0 ; i < 3 ; i++){		
+		//this loop for colomns
 		for(j=0 ; j < 2 ; j++){
 			cin >> list[i][j];
 		}
 	}
+	//this loop for rows
 	for(i = 0 ; i < 3 ; i++){
+		//this loop for colomns
 		for(j=0 ; j < 2 ; j++){
 			cout << list[i][j]<<" ";
 		}
