269e625d7666c79dd6e76f026aee43b25034aba0
Hexagon16 <hexagon16rpm@yandex.com>
2019-12-23 23:26:12 -0800

    modified @ Tue 24 Dec 09:27:00 EET 2019

--- 2dim_array_e.g.cpp
+++ 2dim_array_e.g.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int  main(){
 	int list[3][2];
 	int j ,i;
-	cout << "enter some numerical vsalus \n";
+	cout << "enter some numerical values \n";
 	//this loop for rows
 	for(i = 0 ; i < 3 ; i++){		
 		//this loop for colomns
@@ -18,5 +18,15 @@ int  main(){
 			cout << list[i][j]<<" ";
 		}
 	}
+	
+	/*
+	to output values as matrix
+	for (i = 0; i < 3; i++) {
+		for (j = 0; j < 4; j++) {
+			cout << list[i][j] << " ";
+		}
+		cout << endl;
+
+	}*/
 	return 0;
 }
