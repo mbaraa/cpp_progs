40dd7c1eafd3e79e99310cb059c85ffb90f04cf1
Baraa Al-Masri <hexagon16rpm@yandex.com>
2020-08-19 04:13:33 +0300

    fail 1

--- The Tetris Project/main.c
+++ The Tetris Project/main.c
@@ -1,12 +1,77 @@
 #include "tetrisFuncs.h"
 
 int main() {
+    int rows = 20;
+    int columns = 10;
 
-    char m[20][10] = {{}};
+    char tetrisMainMap[rows][columns];
+    // mark each taken place with true(1)
+    bool checkList[rows][columns];
+    // columns lengths
+    int colsLengths[columns];//
 
-    initMatrix(m, 20, 10);
+    initLengths(colsLengths);
 
-    dropOverColumn(m, 20, 10);
+    initMatrix(*tetrisMainMap, rows, columns);
+
+    //dropOverColumn(m, 20, 10);
+
+    // start dropping from the first row
+    int droppingRow = -1;
+    // dropping starts from the middle
+    int col = columns / 2;
+    // print current tetris
+    printMatrix(*tetrisMainMap, rows, columns);
+    
+/*    for(int i = 0; i < 10; i++) {
+        printf("%d ", colsLengths[i]);
+
+    }
+*/
+    while( true ) {//droppingRow < rows - 1 ) {
+
+        // overlapping checker
+        if( droppingRow == colsLengths[col] ) {
+            checkMap(*tetrisMainMap, *checkList, rows, columns, colsLengths);
+        }
+
+        
+
+        // get current character from keyboard
+        char chr = getch();
+        // replace dropped character with a dot
+        tetrisMainMap[droppingRow++][col] = '.';
+        
+        if( checkList[droppingRow ][col] ){
+            droppingRow = 0;
+            continue;
+        }
+
+        // move left / right
+        if(chr == 'A' || chr == 'a'){
+            col--;
+        } else if(chr == 'D' || chr == 'd'){
+            col++;
+        }
+
+        // setting boundaries
+        if( col >= 9 ){
+            col = 9;
+        } else if( col <= 0 ) {
+            col = 0;
+        }
+
+        // add the dropping chararecter
+        tetrisMainMap[droppingRow][col] = '#';
+
+        clear();
+        
+        
+        // print current tetris map
+        printMatrix(*tetrisMainMap, rows, columns);
+
+        
+    }
 
     return 0;
 
--- The Tetris Project/tetrisFuncs.h
+++ The Tetris Project/tetrisFuncs.h
@@ -9,7 +9,36 @@
 
 #define true 1
 #define false 0
+#define bool int
 
+void initLengths(int *lengths0) {
+    int (*lengths)[10] = lengths0;
+    for(int i = 0; i < 10; i++) {
+        (*lengths)[i] = 9;
+    }
+}
+
+void checkMap(char *matrix0, bool *checkList0, int rows, int columns, int *lengths0) {
+
+    char (*matrix)[columns] = matrix0;
+    bool (*checkList)[columns] = checkList0;
+    int (*lengths)[columns] = lengths0;
+
+    for(int row = 0; row < rows; row++) {
+        
+        for(int col = 0; col < columns; col++){
+
+            if( matrix[row][col] != '.' ) {
+                checkList[row][col] = 1;
+                (*lengths)[col]--;
+                
+            }
+
+        }
+    
+    }
+
+}
 
 void dropOverColumn(char *matrix0, int rows, int columns) {
     // convert the given pointer to matrix
@@ -19,7 +48,7 @@ void dropOverColumn(char *matrix0, int rows, int columns) {
     // dropping starts from the middle
     int col = columns / 2;
     // print current tetris
-    printMatrix(matrix, rows, columns);
+    printMatrix(*matrix, rows, columns);
     
     while( droppingRow < rows - 1 ) {
         
@@ -33,9 +62,20 @@ void dropOverColumn(char *matrix0, int rows, int columns) {
             col--;
         
         } else if(chr == 'D' || chr == 'd'){
-        
+            
             col++;
         
+        }
+
+        // setting boundaries
+        if( col >= 9 ){
+        
+            col = 9;
+        
+        } else if( col <= 0 ) {
+
+            col = 0;
+
         }
 
 
